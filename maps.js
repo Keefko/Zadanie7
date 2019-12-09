75bcb7f1fc49c00362d63dd39e064f81d3857907
Dan Kohout <kohooutdan@gmail.com>
2019-12-09 18:17:43 +0100

    Arne chod do pice

---
+++ maps.js
@@ -0,0 +1,376 @@
+var geojsonFeature = {
+	  "type": "FeatureCollection",
+	  "features": [
+	    {
+	      "type": "Feature",
+	      "properties": {
+	      	"name" : "BLOK A",
+	      	"popupContent": "<ul><li><strong>BLOK A<\/strong><\/li><li>Inštitút komunikácie a aplikovanej lingvistiky<\/li><li>Ústav jadrového a fyzikálneho inžinierstva<\/li><\/ul>"
+	      },
+	      "geometry": {
+	        "type": "Polygon",
+	        "coordinates": [
+	          [
+	            [
+	              17.072550058364868,
+	              48.15181466811292
+	            ],
+	            [
+	              17.073858976364136,
+	              48.15181466811292
+	            ],
+	            [
+	              17.073858976364136,
+	              48.151964982562305
+	            ],
+	            [
+	              17.072550058364868,
+	              48.151964982562305
+	            ],
+	            [
+	              17.072550058364868,
+	              48.15181466811292
+	            ]
+	          ]
+	        ]
+	      }
+	    },
+	    {
+	      "type": "Feature",
+	      "properties": {
+	      	"name" : "BLOK B",
+	      	"popupContent": "<ul><li><strong>BLOK B<\/strong><\/li><li>Ústav elektrotechniky<\/li><li>Ústav multimediálnych informačných a komunikačných technológií<\/li><\/ul>"
+	      },
+	      "geometry": {
+	        "type": "Polygon",
+	        "coordinates": [
+	          [
+	            [
+	              17.07299530506134,
+	              48.152322872336256
+	            ],
+	            [
+	              17.074368596076965,
+	              48.152322872336256
+	            ],
+	            [
+	              17.074368596076965,
+	              48.15246244867156
+	            ],
+	            [
+	              17.07299530506134,
+	              48.15246244867156
+	            ],
+	            [
+	              17.07299530506134,
+	              48.152322872336256
+	            ]
+	          ]
+	        ]
+	      }
+	    },
+	    {
+	      "type": "Feature",
+	      "properties": {
+	      	"name" : "BLOK C",
+	        "popupContent": "<ul><li><strong>BLOK C<\/strong><\/li><li>Ústav elektroenergetiky a aplikovanej elektrotechniky<\/li><li>Ústav informatiky a matematiky<\/li><\/ul>"
+	      },
+	      "geometry": {
+	        "type": "Polygon",
+	        "coordinates": [
+	          [
+	            [
+	              17.072818279266357,
+	              48.152831071526535
+	            ],
+	            [
+	              17.073880434036255,
+	              48.152831071526535
+	            ],
+	            [
+	              17.073880434036255,
+	              48.15296706763929
+	            ],
+	            [
+	              17.072818279266357,
+	              48.15296706763929
+	            ],
+	            [
+	              17.072818279266357,
+	              48.152831071526535
+	            ]
+	          ]
+	        ]
+	      }
+	    },
+	    {
+	      "type": "Feature",
+	      "properties": {
+			"name" : "BLOK D",
+			"popupContent": "<ul><li><strong>BLOK D<\/strong><\/li><li>Ústav robotiky a kybernetiky <\/li><li>Ústav automobilovej mechatroniky<\/li><\/ul>"
+	      },
+	      "geometry": {
+	        "type": "Polygon",
+	        "coordinates": [
+	          [
+	            [
+	              17.073193788528442,
+	              48.15332137160874
+	            ],
+	            [
+	              17.074363231658936,
+	              48.15332137160874
+	            ],
+	            [
+	              17.074363231658936,
+	              48.1534752604496
+	            ],
+	            [
+	              17.073193788528442,
+	              48.1534752604496
+	            ],
+	            [
+	              17.073193788528442,
+	              48.15332137160874
+	            ]
+	          ]
+	        ]
+	      }
+	    },
+	    {
+	      "type": "Feature",
+	      "properties": {
+	      	"name" : "BLOK E",
+	      	"popupContent": "<ul><li><strong>BLOK E<\/strong><\/li><li>Ústav elektroniky a fotoniky <\/li><\/ul>"
+	      	},
+	      "geometry": {
+	        "type": "Polygon",
+	        "coordinates": [
+	          [
+	            [
+	              17.072823643684387,
+	              48.15384387602879
+	            ],
+	            [
+	              17.073880434036255,
+	              48.15384387602879
+	            ],
+	            [
+	              17.073880434036255,
+	              48.15397986945726
+	            ],
+	            [
+	              17.072823643684387,
+	              48.15397986945726
+	            ],
+	            [
+	              17.072823643684387,
+	              48.15384387602879
+	            ]
+	          ]
+	        ]
+	      }
+	    },
+	       {
+      "type": "Feature",
+      "properties": {
+      	"name" : "bus",
+      	"popupContent" : "ZOO 39 - Lafranconi , 31 - Lafranconi"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.075141072273254,
+          48.15413375632322
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"name" : "bus",
+      	"popupContent" : "ZOO 39 - Slávičie údolie , 31 - Slávičie údolie"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.074813842773438,
+          48.15446300109103
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"name" : "bus",
+      	"popupContent" : "ZOO dont"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.07675039768219,
+          48.15395481806331
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"name" : "bus",
+      	"popupContent" : "ZOO 32 - Kramáre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.075843811035156,
+          48.15418743767951
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"name" : "train",
+      	"popupContent" : "Botanická"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.07197070121765,
+          48.14811035149082
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"name" : "train",
+      	"popupContent" : "Botanická"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.071568369865417,
+          48.1481747766733
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"name" : "bus",
+      	"popupContent" : "Botanická dont"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.072373032569885,
+          48.14794928818086
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"name" : "bus",
+      	"popupContent" : "Botanická dont"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.071101665496826,
+          48.14834299760175
+        ]
+      }
+    }
+	]
+};
+
+var trainIcon = L.icon({
+	iconUrl: 'train.png',
+	iconSize: [32,37],
+	iconAnchor: [16,37],
+	popupAnchor: [0,-30]
+});
+
+
+var busIcon = L.icon({
+	iconUrl: 'bus.png',
+	iconSize: [32,37],
+	iconAnchor: [16,37],
+	popupAnchor: [0,-30]
+});
+
+
+function onEachFeature(feature, layer) {
+
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+
+function polystyle(feature) {
+
+	switch(feature.properties.name){
+		case 'BLOK A' :  return { fillColor: 'blue',color: 'darkblue'};
+		case 'BLOK B' :  return { fillColor: 'green',color: 'darkgreen'};
+		case 'BLOK C' :  return { fillColor: 'purple',color: 'darkmagenta'};
+		case 'BLOK D' :  return { fillColor: 'cyan',color: 'darkcyan'};
+		case 'BLOK E' :  return { fillColor: 'orange',color: 'darkorange'};
+	}
+
+}
+
+
+function setIcon(feature, latlng){
+
+	switch(feature.properties.name){
+		case 'train' : return L.marker(latlng, {icon: trainIcon});
+		case 'bus' : return L.marker(latlng, {icon: busIcon});
+	}
+
+}
+
+
+
+
+window.onload = function(){	
+	var mymap = L.map('mapid').setView([48.1518532, 17.073645], 16);
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiMTdkYW55OSIsImEiOiJjazN5YW50ZXgweTluM2RwN2p1ZnpsbjFpIn0.8DTWYaOEHucqOgfoojSsKQ', {
+	    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	    maxZoom: 18,
+	    id: 'mapbox/streets-v11',
+	    accessToken: 'pk.eyJ1IjoiMTdkYW55OSIsImEiOiJjazN5YW50ZXgweTluM2RwN2p1ZnpsbjFpIn0.8DTWYaOEHucqOgfoojSsKQ'
+	}).addTo(mymap);
+
+	L.geoJSON(geojsonFeature,{
+		onEachFeature: onEachFeature,
+		style: polystyle,
+		pointToLayer : setIcon
+	}).addTo(mymap);
+
+
+	var control = L.Routing.control({
+	        router: L.routing.mapbox('pk.eyJ1IjoiMTdkYW55OSIsImEiOiJjazN5YW50ZXgweTluM2RwN2p1ZnpsbjFpIn0.8DTWYaOEHucqOgfoojSsKQ'),
+	     	 waypoints: [
+		        L.latLng(),
+		        L.latLng(48.1518532, 17.073645)
+		    ],
+	        routeWhileDragging: true,
+	        geocoder: L.Control.Geocoder.nominatim()
+	    })
+	    .on('routingerror', function(e) {
+	        try {
+	            mymap.getCenter();
+	        } catch (e) {
+	            mymap.fitBounds(L.latLngBounds(control.getWaypoints().map(function(wp) { return wp.latLng; })));
+	        }
+
+	        handleError(e);
+	    })
+	    .addTo(mymap);
+
+	L.Routing.errorControl(control).addTo(mymap);
+
+}
